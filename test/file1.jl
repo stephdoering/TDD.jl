7de43a8aa923436d5303a56f7c07e34b9da4f943
Stephanie Doering <stephanie@thedoerings.net>
2021-10-25 16:03:56 -0500

    Step2 tests

--- test/file1.jl
+++ test/file1.jl
@@ -32,7 +32,35 @@ using Test
     @test TDD.returnConnectedNode(graph2, 4) == [2,4,5]
     @test TDD.returnConnectedNode(graph2, 5) == [4,5]
 
+    
+    #STEP 2
+    graph3 = [ #directed, not self-referential
+        [2,3],
+        [],
+        [4,5],
+        [2,3],
+        [3]
+    ]
 
+    #given graph and starting node
+    #return list of all nodes reachable from node
+    @test TDD.returnAllReachableNode(graph1, 1) == [1,2,3]
+    @test TDD.returnAllReachableNode(graph1, 2) == [1,2,3]
+    @test TDD.returnAllReachableNode(graph1, 3) == [1,2,3]
+    @test TDD.returnAllReachableNode(graph1, 4) == [4,5]
+    @test TDD.returnAllReachableNode(graph1, 5) == [4,5]
+
+    @test TDD.returnAllReachableNode(graph2, 1) == [1,2,3,4,5]
+    @test TDD.returnAllReachableNode(graph2, 2) == [1,2,3,4,5]
+    @test TDD.returnAllReachableNode(graph2, 3) == [1,2,3,4,5]
+    @test TDD.returnAllReachableNode(graph2, 4) == [1,2,3,4,5]
+    @test TDD.returnAllReachableNode(graph2, 5) == [1,2,3,4,5]
+
+    @test TDD.returnAllReachableNode(graph3, 1) == [1,2,3,4,5]
+    @test TDD.returnAllReachableNode(graph3, 2) == [2]
+    @test TDD.returnAllReachableNode(graph3, 3) == [2,3,4,5]
+    @test TDD.returnAllReachableNode(graph3, 4) == [2,3,4,5]
+    @test TDD.returnAllReachableNode(graph3, 5) == [2,3,4,5]
 
 
 end
