db2ecdd4553e2cd15fab37f18674780bd95ac555
Stephanie Doering <stephanie@thedoerings.net>
2021-10-25 15:53:45 -0500

    implement step1

--- src/TDD.jl
+++ src/TDD.jl
@@ -1,5 +1,12 @@
 module TDD
 
-# Write your package code here.
+    #Step 1
+    function returnConnectedNode(graph::Vector{Vector{Int64}}, node::Int64)
+        result = graph[node]
+        if !(node in result)
+            push!(result, node)
+        end
+        return sort(result)
+    end
 
 end
